174b664a58ebadad4adfb96416b75736576168b4
Julien Saurel <julien.saurel11@gmail.com>
2018-12-04 17:40:56 +0100

    fix

--- view/menu.php
+++ view/menu.php
@@ -1,9 +1,8 @@
 <nav>
 <ul id="menu">
 <li class="accueil"><a href="?action=homepage&controller=accueil">Accueil</a></li>
-<li class="nousConnaitre"><a href="?action=display1st&controller=nousConnaitre">Nos Produits</a></li>
-
-<li class="Panier"><a href="?action=display&controller=Panier">Panier</a></li>
+<li class="nousConnaitre"><a href="?action=display&controller=planetes">Nos Produits</a></li>
+<li class="monProfil"><a href="?action=display1st&controller=monProfil">Mon profil</a></li>
 
 
 <?php  if (!isset($_SESSION['login'])) { ?>
