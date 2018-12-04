f0629bbe47e08091fe62c58cfc5d0d0451ab8257
Louise R <louiserobert2510@gmail.com>
2018-12-04 17:37:27 +0100

    fix

--- view/menu.php
+++ view/menu.php
@@ -3,7 +3,7 @@
 <li class="accueil"><a href="?action=homepage&controller=accueil">Accueil</a></li>
 
 <li class="nousConnaitre"><a href="?action=display&controller=planetes">Nos Produits</a></li>
-<li class="monProfil"><a href="?action=display1st&controller=monProfil">Mon profil</a></li>
+<li class="monProfil"><a href="?action=profile&controller=monProfil">Mon profil</a></li>
 
 <?php  if (!isset($_SESSION['login'])) { ?>
         <li class="SeConnecter">
