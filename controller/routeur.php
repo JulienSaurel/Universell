9b30e4047dfca3fcfa700c874910b25844af4904
David Sambuc <dsambuc@free.fr>
2018-12-05 14:58:38 +0100

    suppression de code inutile

--- controller/routeur.php
+++ controller/routeur.php
@@ -9,41 +9,38 @@ require_once File::build_path(array('controller','ControllerMonProfil.php'));
 
 //------------controller-------------
 if(!isset($_GET['controller'])) //Si le controller n'a  pas été spécifiée
-	{
-		$controller = 'accueil'; //On définit un controller par defaut (Personne)
-	}
+{
+    $controller = 'accueil'; //On définit un controller par defaut (Personne)
+}
 
-	else
-	{
-		if(!($_GET['controller'] == 'monProfil')||isset($_SESSION['login'])) {
-            $controller = $_GET['controller']; // On recupère le controller passée dans l'URL
-        }
-		else {
-			$controller = 'accueil';
-			$action = 'error';
-		}
-	}
+else
+{
+    if(!($_GET['controller'] == 'monProfil')||isset($_SESSION['login'])) {
+        $controller = $_GET['controller']; // On recupère le controller passée dans l'URL
+    }
+    else {
+        $controller = 'accueil';
+        $action = 'error';
+    }
+}
 
-$controller_class = 'Controller' . ucfirst($controller); 
+$controller_class = 'Controller' . ucfirst($controller);
 //on crée la variable qui represente la classe dur laquelle on appellera l'action
 
 //--------------action---------------
-$action = null;
-if(!$action) {
-    if (!isset($_GET['action'])) //Si l'action n'a  pas été spécifiée
-    {
-        $action = 'homepage'; //On définit une action par defaut (readAll)
+if (!isset($_GET['action'])) //Si l'action n'a  pas été spécifiée
+{
+    $action = 'homepage'; //On définit une action par defaut (readAll)
+} else {
+    if (in_array($_GET['action'], get_class_methods($controller_class))) {
+        $action = $_GET['action']; // On recupère l'action passée dans l'URL
     } else {
-        if (in_array($_GET['action'], get_class_methods($controller_class))) {
-            $action = $_GET['action']; // On recupère l'action passée dans l'URL
-        } else {
-            $action = 'error';
-        }
-
+        $action = 'error';
     }
+
 }
+
 $controller_class::$action();
 // Appel de la méthode statique $action de ControllerPersonne
 
 ?>
-
