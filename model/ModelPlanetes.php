d33024508970a0c5293f3f4be4c226984e2d527a
David Sambuc <dsambuc@free.fr>
2018-12-05 16:19:14 +0100

    debut admin

--- model/ModelPlanetes.php
+++ model/ModelPlanetes.php
@@ -13,71 +13,7 @@ class ModelPlanetes extends Model
     static protected $object = 'planetes';
     protected static $primary='id';
 
-    // Getter générique
-    /*public function get($nom_attribut)
-    {
-        if (property_exists($this, $nom_attribut))
-            return $this->$nom_attribut;
-        return false;
-    }
 
-    // Setter générique
-    public function set($nom_attribut, $valeur)
-    {
-        if (property_exists($this, $nom_attribut))
-            $this->$nom_attribut = $valeur;
-        return false;
-    }
-
-    // un constructeur
-
-    public function __construct($nom = NULL, $prix = NULL, $qteStock = NULL, $image = NULL )
-    {
-        if (!is_null($nom) && !is_null($prix) && !is_null($qteStock)) {
-
-            $this->nom = $nom;
-            $this->prix = $prix;
-            $this->qteStock = $qteStock;
-            $this->image = $image;
-        }
-    }
-
-    
-
-    public function save()
-    {
-        $sql = "INSERT INTO uni_Planete (nom, prix, qteStock, image) VALUES (:nom, :prix, :qteStock, :image)";
-
-        // Préparation de la requête
-        $req_prep = Model::$pdo->prepare($sql);
-
-
-        $values = array(
-            "nom" => $this->nom,
-            "prix" => $this->prix,
-            "qteStock" => $this->qteStock,
-            "image" => $this->image,
-	    );
-        // On donne les valeurs et on exécute la requête
-        $req_prep->execute($values);
-    }*/
-
-    public static function getPlaneteById($id){
-        $sql = "SELECT * from uni_Planetes WHERE id=:id";
-
-        $req_prep = Model::$pdo->prepare($sql);
-
-        $values = array(
-            'id' => $id );
-        //var_dump($sql);
-        
-        $req_prep->execute($values);
-
-        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelPlanetes');
-        $tab = $req_prep->fetchAll();
-
-        return $tab[0];
-    }
     
 }
 ?>
--- view/planetes/infoPlanete.php
+++ view/planetes/infoPlanete.php
@@ -1,3 +1,6 @@
-<h1> info planete </h1>
+<h1> La planète <?php echo $planete->get('nom'); ?></h1>
 
-<?php echo "<img src=". $planete->get('image')." alt=\" planete \" >"?>
+<?php echo "<img src=". $planete->get('image')." alt=\" planete \" >";
+    $id = $planete->get('id');
+    echo "<a href=\"?action=delete&id={$id}&controller=planetes\">Supprimer</a>";
+    ?>
