7b4499878b757e5466c01380aaba8189163949c9
Julien Saurel <julien.saurel11@gmail.com>
2018-12-13 17:27:22 +0100

    fixes

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -2,7 +2,7 @@
 <a href="index.php?action=connect&controller=client"> Se connecter </a>
 <p> Vous souhaitez vous inscrire ? cliquez <a href="index.php?action=create&controller=client"> ici</a>.</p>
 <h2> Bienvenue sur le 1er site de ventes de planètes ! </h2>
-<p> Bonne navigation sur notre site ! cliquez <a href="index.php?action=display&controller=client">ici </a>pour 
+<p> Bonne navigation sur notre site ! cliquez <a href="index.php?action=display&controller=planetes">ici </a>pour 
 	consulter nos produits.</p> 
 
 
