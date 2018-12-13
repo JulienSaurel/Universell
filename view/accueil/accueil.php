6e3789f93c6cb6bb1c9482baa5036b345157cf45
Julien Saurel <julien.saurel11@gmail.com>
2018-12-13 17:04:13 +0100

    page d'accueil

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,4 +1,8 @@
-<p> Accueil
-<a href="index.php?action=create&controller=client">Create</a> 
-</p>
+<h1> Accueil </h1>
+<a href="index.php?action=connect&controller=client"> Se connecter </a>
+<p> Vous souhaitez vous inscrire ? cliquez <a href="index.php?action=create&controller=client"> ici</a>.</p>
+<h2> Bienvenue sur le 1er site de ventes de planètes ! </h2>
+<p> Bonne navigation sur notre site ! cliquez <a href="index.php?action=display&controller=client">ici </a>pour 
+	consulter nos produits.</p> 
+
 
